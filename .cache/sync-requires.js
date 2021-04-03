c9e88c6c7848817e876a8faea28a27c9ded40144
Haroon Choudery <haroonchoudery@Haroons-MacBook-Pro.local>
2021-04-03 02:47:43 -0400

    changed url back

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,12 +5,8 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": (preferDefault(require("/Users/haroonchoudery/GitLab/haroon-helps/gatsby-starter-ghost/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": (preferDefault(require("/Users/haroonchoudery/GitLab/haroon-helps/gatsby-starter-ghost/src/pages/404.js"))),
-  "component---src-templates-author-js": (preferDefault(require("/Users/haroonchoudery/GitLab/haroon-helps/gatsby-starter-ghost/src/templates/author.js"))),
-  "component---src-templates-index-js": (preferDefault(require("/Users/haroonchoudery/GitLab/haroon-helps/gatsby-starter-ghost/src/templates/index.js"))),
-  "component---src-templates-page-js": (preferDefault(require("/Users/haroonchoudery/GitLab/haroon-helps/gatsby-starter-ghost/src/templates/page.js"))),
-  "component---src-templates-post-js": (preferDefault(require("/Users/haroonchoudery/GitLab/haroon-helps/gatsby-starter-ghost/src/templates/post.js"))),
-  "component---src-templates-tag-js": (preferDefault(require("/Users/haroonchoudery/GitLab/haroon-helps/gatsby-starter-ghost/src/templates/tag.js")))
+  "component---cache-dev-404-page-js": (preferDefault(require("/Users/haroonchoudery/GitLab/haroon-helps/haroon-helps/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": (preferDefault(require("/Users/haroonchoudery/GitLab/haroon-helps/haroon-helps/src/pages/404.js"))),
+  "component---src-templates-index-js": (preferDefault(require("/Users/haroonchoudery/GitLab/haroon-helps/haroon-helps/src/templates/index.js")))
 }
 
